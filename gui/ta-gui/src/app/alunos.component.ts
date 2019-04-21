873ba97c4212cf1ea743d557e3027d635ce5b3c9
Juliano <jctvEC@github.com>
2019-04-20 23:28:06 -0300

    Servidor integrado com Gui, git Ignore e modificações de legibilidade

--- gui/ta-gui/src/app/alunos.component.ts
+++ gui/ta-gui/src/app/alunos.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 import { NgModule } from '@angular/core';
 
@@ -17,12 +18,16 @@ export class AlunosComponent implements OnInit {
    cpfduplicado: boolean = false;
 
    criarAluno(a: Aluno): void {
-     if (this.alunoService.criar(a)) {
-       this.alunos.push(a);
-       this.aluno = new Aluno();
-     } else {
-       this.cpfduplicado = true;
-     }
+     this.alunoService.criar(a)
+        .then(ab => {
+           if (ab) {
+              this.alunos.push(ab);
+              this.aluno = new Aluno();
+           } else {
+              this.cpfduplicado = true;
+           }
+        })
+        .catch(erro => alert(erro));
    }
 
    onMove(): void {
@@ -30,7 +35,9 @@ export class AlunosComponent implements OnInit {
    }
 
    ngOnInit(): void {
-     this.alunos = this.alunoService.getAlunos();
+     this.alunoService.getAlunos()
+         .then(as => this.alunos = as)
+         .catch(erro => alert(erro));
    }
 
 }
